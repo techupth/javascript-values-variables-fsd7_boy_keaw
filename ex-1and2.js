6b65fa4a6c6da7239994e57690e1ce2702b04ed5
thanapakorn <thanapakorn001@gmail.com>
2024-09-30 13:38:11 +0700

    JavaScript Values Variables Assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
+let firstName = "Karter" ;
+let lastName = "Aderson" ;
+let contact = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
 //Start coding here
+const restaurantNameList = "Chef's Table by Chef James" ;
+const netFoodPrice = 15000 ;
+const openRestaurantNameList = 1500 ;
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+contact = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+netFoodPrice = 5000;
+openRestaurantNameList = 6000;
+
--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,7 @@
 // Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
-
+celsius = (celsius*1.8) + 32 ;
 // Start coding here;
-
-let fahrenheit;
+let fahrenheit ;
+fahrenheit = celsius ;
+console.log(fahrenheit);
